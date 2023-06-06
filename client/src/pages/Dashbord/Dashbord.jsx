e2437360d4a39ab2b8cc6e8637330c4cf4d9e097
Samael Marjan <samaaelmarjan@gmail.com>
2023-06-06 11:50:24 +0600

    edit profile

--- client/src/pages/Dashbord/Dashbord.jsx
+++ client/src/pages/Dashbord/Dashbord.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './dashbord.css'
 import { useSelector } from 'react-redux'
+import {FcSettings} from 'react-icons/fc'
+import { useNavigate } from 'react-router-dom'
 
 const Dashbord = () => {
+    const navigate = useNavigate()
     const {user} = useSelector((state) => state.auth)
   return (
     <div className='dashbord'>
-        <div className='container-fluid'>
+        <div className='container-fluid dashbord-body'>
             <div className='container mt-5 profile'>
                 <div className='image border'>
                     <img src='' alt='/' />
@@ -15,6 +18,9 @@ const Dashbord = () => {
                     <p><span>User Name:</span>  {user?.username}</p>
                     <p><span>Email Address:</span>   {user?.email}</p>
                 </div>
+            </div>
+            <div className='dash-setting' onClick={() => navigate('/profile')}>
+                <FcSettings size={25} /> <span>Edit profile</span>
             </div>
         </div>
     </div>
